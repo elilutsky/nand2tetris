c77e7cac80e86fd82f51d491f04a3225687ab5a2
Denis\DenisPC <denisgolyanov3@gmail.com>
2020-05-23 09:51:42 -0700

    renamed main according to submission requirements

--- projects/07/VMTranslator/main.py
+++
@@ -1,8 +0,0 @@
-import argparse
-from vm_translator import translator
-
-if __name__ == '__main__':
-    argsparser = argparse.ArgumentParser(description='VMTranslator for the Hack language.')
-    argsparser.add_argument('file', metavar='FILE', help='path to the .vm file')
-    args = argsparser.parse_args()
-    translator.translate(args.file)
