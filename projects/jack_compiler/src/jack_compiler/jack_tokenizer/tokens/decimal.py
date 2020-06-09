70ac4b5c8205d221c4d671665b5bff4871e78d4f
Eli Lutsky <eli3143@gmail.com>
2020-06-09 12:37:17 +0300

    Removed code from decimal token

--- projects/jack_compiler/src/jack_compiler/jack_tokenizer/tokens/decimal.py
+++ projects/jack_compiler/src/jack_compiler/jack_tokenizer/tokens/decimal.py
@@ -2,34 +2,12 @@ import re
 
 from .base import JackToken
 
-_DECIMAL_RANGE_REGEX = re.compile(
-    '^(3276[0-7]|327[0-5][0-9]|32[0-6][0-9][0-9]|3[0-1][0-9][0-9][0-9]|'  # Match 5 digit numbers
-    '1000[0-9]|100[1-9][0-9]|10[1-9][0-9][0-9]|1[1-9][0-9][0-9][0-9]|[2-2][0-9][0-9][0-9][0-9]|'  # Match 5 digit numbers
-    '999[0-9]|99[0-8][0-9]|9[0-8][0-9][0-9]|100[0-9]|10[1-9][0-9]|1[1-9][0-9][0-9]|[2-8][0-9][0-9][0-9]|'  # Match 4 digit numbers
-    '99[0-9]|9[0-8][0-9]|10[0-9]|1[1-9][0-9]|[2-8][0-9][0-9]|'  # Match 3 digit numbers
-    '9[0-9]|1[0-9]|[2-8][0-9]|[0-9])'  # Match 1-2 digit numbers
-)
-
 
 class JackDecimal(JackToken):
-
-    def __init__(self, number):
-        self._number = number
-
-    @staticmethod
-    def tokenize(word) -> ('JackDecimal', str):
-        match = _DECIMAL_RANGE_REGEX.match(word)
-        if match:
-            return JackDecimal(match.group(1)), word[len(match.group(0)):]
-        else:
-            return None, word
-
-    @property
-    def value(self):
-        return int(self._number)
-
-    def __eq__(self, other: 'JackDecimal'):
-        return self.value == other.value
-
-    def __repr__(self):
-        return f'<JackDeciaml.NUMBER: \'{self.value}\'>'
+    _TOKEN_REGEX = re.compile(
+        '^(3276[0-7]|327[0-5][0-9]|32[0-6][0-9][0-9]|3[0-1][0-9][0-9][0-9]|'  # Match 5 digit numbers
+        '1000[0-9]|100[1-9][0-9]|10[1-9][0-9][0-9]|1[1-9][0-9][0-9][0-9]|[2-2][0-9][0-9][0-9][0-9]|'  # Match 5 digit numbers
+        '999[0-9]|99[0-8][0-9]|9[0-8][0-9][0-9]|100[0-9]|10[1-9][0-9]|1[1-9][0-9][0-9]|[2-8][0-9][0-9][0-9]|'  # Match 4 digit numbers
+        '99[0-9]|9[0-8][0-9]|10[0-9]|1[1-9][0-9]|[2-8][0-9][0-9]|'  # Match 3 digit numbers
+        '9[0-9]|1[0-9]|[2-8][0-9]|[0-9])'  # Match 1-2 digit numbers
+    )
