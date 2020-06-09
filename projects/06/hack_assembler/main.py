0f0eca31e82ee7a50f2d67fd30a7e087b4a59343
Eli Lutsky <eli3143@gmail.com>
2020-06-09 09:10:32 +0300

    Fix project 6

--- projects/06/hack_assembler/main.py
+++
@@ -1,8 +0,0 @@
-import argparse
-from hack_assembler import assembler
-
-if __name__ == '__main__':
-    argsparser = argparse.ArgumentParser(description='Assembler for Hack language.')
-    argsparser.add_argument('file', metavar='FILE', help='path to the .asm file')
-    args = argsparser.parse_args()
-    assembler.assemble(args.file)
