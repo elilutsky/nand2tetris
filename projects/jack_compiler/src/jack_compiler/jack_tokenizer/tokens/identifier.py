3aaf058bfd46254a00c427d51a5c1f43a00a6579
Eli Lutsky <eli3143@gmail.com>
2020-06-09 12:37:42 +0300

    Removed code from identifier token

--- projects/jack_compiler/src/jack_compiler/jack_tokenizer/tokens/identifier.py
+++ projects/jack_compiler/src/jack_compiler/jack_tokenizer/tokens/identifier.py
@@ -2,28 +2,6 @@ import re
 
 from .base import JackToken
 
-_IDENTIFIER_REGEX = re.compile(r'^([^\d\W][\w_]*)')
-
 
 class JackIdentifier(JackToken):
-
-    def __init__(self, identifier):
-        self._identifier = identifier
-
-    @staticmethod
-    def tokenize(word) -> ('JackIdentifier', str):
-        match = _IDENTIFIER_REGEX.match(word)
-        if match:
-            return JackIdentifier(match.group(1)), word[len(match.group(0)):]
-        else:
-            return None, word
-
-    @property
-    def value(self):
-        return self._identifier
-
-    def __eq__(self, other: 'JackIdentifier'):
-        return self.value == other.value
-
-    def __repr__(self):
-        return f'<JackIdentifier.IDENTIFIER: \'{self.value}\'>'
+    _TOKEN_REGEX = re.compile(r'^([^\d\W][\w_]*)')
